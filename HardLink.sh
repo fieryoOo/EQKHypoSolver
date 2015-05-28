9fc6d3f44c66747e3d41c6d66a346707aa0fcd1c
Ye Tian <tianye@spice.colorado.edu>
2015-05-28 14:06:28 -0600

    HardLink.sh updated to auto-search for code_dir

--- HardLink.sh
+++ HardLink.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
+# check code directory
+dirs=(${dir_code} /home/tianye/code)
+dir_code='dir_NaN'
+for dir in ${dirs[@]}; do
+	if [ -e $dir ]; then
+		dir_code=$dir
+		break;
+	fi
+done
+if [ $dir_code == "dir_NaN" ]; then
+	echo "cannot locate the code directory!"
+	exit
+fi
+
 # file lists to link
 filelist1=(src_SDContainer/Point.h src_SDContainer/Map.h src_SDContainer/Array2D.h src_SDContainer/DisAzi.h src_SDContainer/Map.cpp src_SDContainer/StaList.h src_MISC/MyOMP.h src_MISC/StackTrace.h src_RadPattern/RadPattern.h src_RadPattern/RadPattern.cpp)
 
-filelist2=(/projects/yeti4009/code/MyLibs/include/Point.h /projects/yeti4009/code/MyLibs/include/Map.h /projects/yeti4009/code/MyLibs/include/Array2D.h /projects/yeti4009/code/MyLibs/include/DisAzi.h /projects/yeti4009/code/MyLibs/sources/Map/Map.cpp /projects/yeti4009/code/MyLibs/include/StaList.h /projects/yeti4009/code/MyLibs/include/MyOMP.h /projects/yeti4009/code/MyLibs/include/StackTrace.h /projects/yeti4009/code/Programs/RadPattern/RadPattern_C/RadPattern.h /projects/yeti4009/code/Programs/RadPattern/RadPattern_C/RadPattern.cpp)
+filelist2=(${dir_code}/MyLibs/include/Point.h ${dir_code}/MyLibs/include/Map.h ${dir_code}/MyLibs/include/Array2D.h ${dir_code}/MyLibs/include/DisAzi.h ${dir_code}/MyLibs/sources/Map/Map.cpp ${dir_code}/MyLibs/include/StaList.h ${dir_code}/MyLibs/include/MyOMP.h ${dir_code}/MyLibs/include/StackTrace.h ${dir_code}/Programs/RadPattern/RadPattern_C/RadPattern.h ${dir_code}/Programs/RadPattern/RadPattern_C/RadPattern.cpp)
 
 ifile=${#filelist1[@]}
 for file1 in `ls ./src_RadPattern/*.f`; do
-	file2=`echo $file1 | awk -F/ '{print "/projects/yeti4009/code/Programs/RadPattern/RadPattern_C/RadPattern_src/"$NF}'`
+	file2=`echo $file1 | awk -v dir=${dir_code} -F/ '{print dir"/Programs/RadPattern/RadPattern_C/RadPattern_src/"$NF}'`
 	filelist1[ifile]=$file1
 	filelist2[ifile]=$file2
 	let ifile++
